43b9a8b430bc633e5afdacc5afac8c33ecc8d29c
Andrew Short <andrewjshort@gmail.com>
2012-10-15 00:23:17 +1100

    Make file uploading return more descriptive error messages.

--- scavenger/code/dataobjects/FileUploadTask.php
+++ scavenger/code/dataobjects/FileUploadTask.php
@@ -12,22 +12,30 @@ class FileUploadTask extends ScavengerTask {
 	}
 	
 	public function processSubmission($data) {
-		if (isset($data['File']) && isset($data['File']['tmp_name'])) {
-			$upload = new Upload();
-			if ($upload->load($data['File'])) {
-				$file = $upload->getFile();
-				if ($file->ID) {
-					$response = $this->newResponse('TaskResponseFileUpload');
-					$response->Response = $file->getAbsoluteURL();
-					$response->UploadedFileID = $file->ID;
-					$response->Status = 'Pending';
-					$response->write();
-
-					return $response;
-				}
-				
-			}
+		if(!isset($data['File']['tmp_name'])) {
+			return 'Please select a file to upload';
 		}
-		return 'Upload could not be saved, please try again';
+
+		$upload = new Upload();
+		$upload->load($data['File']);
+
+		if($upload->isError()) {
+			return sprintf(
+				'Upload could not be saved: %s.',
+				implode(', ', $upload->getErrors())
+			);
+		}
+
+		if(!$file = $upload->getFile()) {
+			return 'Upload could not be saved, please try again.';
+		}
+
+		$response = $this->newResponse('TaskResponseFileUpload');
+		$response->Response = $file->getAbsoluteURL();
+		$response->UploadedFileID = $file->ID;
+		$response->Status = 'Pending';
+		$response->write();
+
+		return $response;
 	}
 }
